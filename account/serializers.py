526814472da901172a775149e9bb443878e48cc8
Hasbik777 <sanjar.turumov@gmail.com>
2023-01-24 19:08:20 +0600

    added ilim lox

--- account/serializers.py
+++ account/serializers.py
@@ -8,24 +8,24 @@ User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(min_length=8, max_length=20, required=True, write_only=True)
+    # password = serializers.CharField(min_length=8, max_length=20, required=True, write_only=True)
     password2 = serializers.CharField(min_length=8, max_length=20, required=True, write_only=True)
 
     class Meta:
         model = User
         fields = ('email', 'password', 'password2', 'first_name', 'last_name', 'username', 'avatar')
 
-        def validate(self, attrs):
-            password2 = attrs.pop('password2')
-            if attrs['password'] != password2:
-                raise serializers.ValidationError('Password did not match')
-            if not attrs['password'].isalnum():
-                raise serializers.ValidationError('Password field must contain alpha and numeric symbol')
-            return attrs
+    def validate(self, attrs):
+        password2 = attrs.pop('password2')
+        if attrs['password'] != password2:
+            raise serializers.ValidationError('Password did not match')
+        if not attrs['password'].isalnum():
+            raise serializers.ValidationError('Password field must contain alpha and numeric symbol')
+        return attrs
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class LogoutSerializer(serializers.Serializer):
